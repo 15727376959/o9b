1055f79570ab07a3fea1080f5a97e4c873ede72a
123 <1463845241@qq.com>
2016-07-16 15:44:19 +0800

    哈哈

--- basic/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    return [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost:3306;dbname=yi',
-        'username' => 'root',
-        'password' => 'root',
-        'charset' => 'utf8',
-        'tablePrefix' => 'wg_',
-    ];
